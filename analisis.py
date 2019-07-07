d5c067ed4af7147395a87d0769470ac4fc5e079f
Eiji Mitsumizo <mitsumizo.eiji@gmail.com>
2019-07-07 22:29:49 +0900

    done

--- analisis.py
+++ analisis.py
@@ -4,6 +4,9 @@ from janome.analyzer import Analyzer
 from janome.tokenizer import Tokenizer
 from janome.tokenfilter import *
 
+import numpy as np
+from scipy import stats
+
 json_file = open('data.json', 'r', encoding='utf-8')
 json_dict = json.load(json_file)
 texts = []
@@ -15,6 +18,7 @@ json_file.close()
 t = Tokenizer()
 
 result = list()
+all = 0
 for rank in range(len(texts)):
     kind = []
     text = t.tokenize(texts[rank],wakati=True)
@@ -23,23 +27,23 @@ for rank in range(len(texts)):
         if not word in kind:
             kind.append(word)
             type += 1
+            all += 1
     result.append({'rank': rank + 1, 'type': type})
 
-for re in result:
-    print(re)
+# print('all: ' + str(all))
+# print('average: ' + str(all / len(texts)))
 
+first = np.empty(50)
+second = np.empty(50)
 
-# texts = '僕は疲れました。僕は教育を変えたいです。'
-# t = Tokenizer()
-#
-# text = t.tokenize(texts, wakati=True)
-#
-# kind = []
-# type = 0
-# for word in text:
-#     if not word in kind:
-#         kind.append(word)
-#         type += 1
-#
-# print(kind)
-# print(type)
+for rank in range(50):
+    first[rank:rank+1] = result[rank]['type']
+
+for index in range(50):
+    second[index:index+1] = result[index + 50]['type']
+
+# print(first)
+# print(second)
+print(stats.ttest_ind(first, second))
+#Ttest_indResult(statistic=0.23367427377675823, pvalue=0.8157252926627823)
+# -> 有意義とはいえない。
